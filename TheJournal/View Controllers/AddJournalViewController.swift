2afcd003a2b86a5c73585c4048faa1d8a3b9dc83
AustinPotts <pottsevolvements@gmail.com>
2019-09-21 22:12:53 -0400

    Configured IB Layout + Subclass Files

---
+++ TheJournal/View Controllers/AddJournalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddJournalViewController.swift
+//  TheJournal
+//
+//  Created by Austin Potts on 9/21/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddJournalViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
